b3e22296e0e78dd94bb8e1dcf0b74b21c8d0c465
BalanaguYashwanth <balanaguyashwanth@gmail.com>
2024-08-24 12:34:51 +0530

    improved patient record api by populating the refs

--- controllers/fetchPatientRecords.js
+++ controllers/fetchPatientRecords.js
@@ -1,10 +1,8 @@
-const http = require('http');
-const { Server } = require("socket.io");
-const patient = require("../models/patientModels")
+const patient = require("../models/patientModels");
 
 const fetchPatientRecords = async () => {
     try {
-        const data = await patient.find();
+        const data = await patient.find().populate('condition').populate('files');
         return data || [];
     } catch (error) {
         throw error
